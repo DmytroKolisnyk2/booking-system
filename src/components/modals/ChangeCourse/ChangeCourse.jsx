1d64b4412ee2d1a07321edd86b7efac05c991bdd
Max Popsuy <maxim09p@gmail.com>
2022-07-26 22:46:48 +0300

    modals refactoring

--- src/components/modals/ChangeCourse/ChangeCourse.jsx
+++ src/components/modals/ChangeCourse/ChangeCourse.jsx
@@ -37,38 +37,3 @@ const NewManager = ({ isOpen, handleClose, id }) => {
 };
 
 export default NewManager;
-// {isOpen && (
-//   <Modal open={isOpen} onClose={handleClose}>
-//     <div className={styles.modal}>
-//       <h3 className={styles.title}>Change course's info</h3>
-//       <form onSubmit={handleSubmit} className={styles.form}>
-//         <label className={styles.input__label}>
-//           <p className={styles.input__label}>Course name:</p>
-//           <input
-//             className={styles.input}
-//             type="text"
-//             name="name"
-//             required
-//             placeholder="Course name"
-//             onChange={(e) => setName(e.currentTarget.value)}
-//           />
-//         </label>
-//         <div className={styles.button__wrapper}>
-//           <button
-//             type="button"
-//             className={styles.input__delete}
-//             onClick={handleDelete}
-//           >
-//             Delete
-//           </button>
-//           <input
-//             className={styles.input__submit}
-//             type="submit"
-//             value="Save"
-//           />
-//         </div>
-//       </form>
-//       <p className={styles.exit}>Click outside to exit</p>
-//     </div>
-//   </Modal>
-// )}
