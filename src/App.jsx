3cfee0fcd7cc7159cd7c087676cf49551cc7af74
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-08-03 14:12:31 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,9 @@ import path from "./helpers/routerPath";
 import ModalsPage from "./pages/ModalsPage/ModalsPage";
 import Footer from "./components/Footer/Footer";
 import AdminPage from "./pages/Admin/AdminPage";
-// import HomePage from "./pages/HomePage/HomePage";
 
 import SuperAdministratorPage from "./pages/SuperAdmin/SuperadminPage";
-import ConfirmatorPage from "./pages/ConfirmatorPage/ConfirmatorPage";
+import ConfirmatorPage from "./pages/Confirmator/ConfirmatorPage";
 
 import AdminUsersPage from "./pages/Admin/UsersPage";
 import AdminGroupsPage from "./pages/Admin/GroupsPage";
@@ -26,31 +25,38 @@ import ManagerPage from "./pages/Manager/ManagerPage";
 import ConsultationsPage from "./pages/Manager/ConsultationsPage";
 import PlanningPage from "./pages/Manager/PlanningPage";
 import HomePage from "./pages/HomePage/HomePage";
+import CallerPage from "./pages/Caller/CallerPage";
 
 class App extends Component {
   render() {
     return (
       <>
         <Routes>
+          <Route path={path.modals} element={<ModalsPage />} />
+
           <Route path={path.admin} element={<AdminPage />}>
             <Route path={path.users} element={<AdminUsersPage />} />
             <Route path={path.groups} element={<AdminGroupsPage />} />
             <Route path={path.courses} element={<AdminCoursesPage />} />
             <Route path={path.actions} element={<AdminActionsPage />} />
           </Route>
+
           <Route path={path.superAdmin} element={<SuperAdministratorPage />}>
             <Route path={path.users} element={<UsersPage />} />
             <Route path={path.groups} element={<GroupsPage />} />
             <Route path={path.courses} element={<CoursesPage />} />
             <Route path={path.actions} element={<ActionsPage />} />
           </Route>
+
           <Route path={path.manager} element={<ManagerPage />}>
             <Route path={path.consultations} element={<ConsultationsPage />} />
             <Route path={path.planning} element={<PlanningPage />} />
           </Route>
 
-          <Route path={path.home} element={<HomePage />} />
-          <Route path={path.modals} element={<ModalsPage />} />
+          <Route path={path.confirmator} element={<ConfirmatorPage />} />
+          <Route path={path.caller} element={<CallerPage />} />
+          
+          <Route path={path.all} element={<HomePage />} />
         </Routes>
         <Footer />
       </>
