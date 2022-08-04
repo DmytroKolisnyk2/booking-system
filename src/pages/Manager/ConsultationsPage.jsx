6da7409f1cfd4e63dc73c10fd8dd7072f4f09fec
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-08-04 11:10:23 +0300

    removed head

--- src/pages/Manager/ConsultationsPage.jsx
+++ src/pages/Manager/ConsultationsPage.jsx
@@ -18,7 +18,7 @@ import {
 } from "../../redux/manager/manager-operations";
 import { updateSlot } from "../../helpers/api";
 import Button from "../../components/Buttons/Buttons";
-import StatusDefinition from "../../components/StatusDefinition/StatusDefinition";  
+import StatusDefinition from "../../components/StatusDefinition/StatusDefinition";
 import DatePicker from "../../components/DatePicker/DatePicker";
 
 const ConsultationPage = () => {
@@ -30,562 +30,550 @@ const ConsultationPage = () => {
   const weekId = useSelector(getWeekId);
   const arrayDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-<<<<<<< HEAD
+  const table = [
+    [
+      {
+        color: 0,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 9,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 10,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 11,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 12,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 13,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 14,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 15,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 16,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 17,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 18,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 14,
+        time: 20,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 21,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 22,
+      },
+    ],
+    [
+      {
+        color: 0,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 9,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 10,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 11,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 12,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 13,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 14,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 15,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 16,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 17,
+      },
+      {
+        color: 1,
+        slot_id: 24,
+        time: 18,
+      },
+      {
+        color: 1,
+        slot_id: 15,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 8,
+        time: 20,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 21,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 22,
+      },
+    ],
+    [
+      {
+        color: 2,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 9,
+      },
+      {
+        color: 2,
+        slot_id: 37,
+        time: 10,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 11,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 12,
+      },
+      {
+        color: 2,
+        slot_id: 32,
+        time: 13,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 14,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 15,
+      },
+      {
+        color: 1,
+        slot_id: 28,
+        time: 16,
+      },
+      {
+        color: 1,
+        slot_id: 23,
+        time: 17,
+      },
+      {
+        color: 1,
+        slot_id: 17,
+        time: 18,
+      },
+      {
+        color: 1,
+        slot_id: 10,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 6,
+        time: 20,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 21,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 22,
+      },
+    ],
+    [
+      {
+        color: 0,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 2,
+        slot_id: 38,
+        time: 9,
+      },
+      {
+        color: 2,
+        slot_id: 39,
+        time: 10,
+      },
+      {
+        color: 2,
+        slot_id: 35,
+        time: 11,
+      },
+      {
+        color: 2,
+        slot_id: 33,
+        time: 12,
+      },
+      {
+        color: 1,
+        slot_id: 31,
+        time: 13,
+      },
+      {
+        color: 1,
+        slot_id: 30,
+        time: 14,
+      },
+      {
+        color: 1,
+        slot_id: 29,
+        time: 15,
+      },
+      {
+        color: 1,
+        slot_id: 27,
+        time: 16,
+      },
+      {
+        color: 1,
+        slot_id: 22,
+        time: 17,
+      },
+      {
+        color: 1,
+        slot_id: 18,
+        time: 18,
+      },
+      {
+        color: 1,
+        slot_id: 11,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 9,
+        time: 20,
+      },
+      {
+        color: 2,
+        slot_id: 4,
+        time: 21,
+      },
+      {
+        color: 2,
+        slot_id: 3,
+        time: 22,
+      },
+    ],
+    [
+      {
+        color: 0,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 9,
+      },
+      {
+        color: 2,
+        slot_id: 36,
+        time: 10,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 11,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 12,
+      },
+      {
+        color: 2,
+        slot_id: 34,
+        time: 13,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 14,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 15,
+      },
+      {
+        color: 1,
+        slot_id: 26,
+        time: 16,
+      },
+      {
+        color: 1,
+        slot_id: 21,
+        time: 17,
+      },
+      {
+        color: 1,
+        slot_id: 19,
+        time: 18,
+      },
+      {
+        color: 1,
+        slot_id: 12,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 5,
+        time: 20,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 21,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 22,
+      },
+    ],
+    [
+      {
+        color: 0,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 9,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 10,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 11,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 12,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 13,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 14,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 15,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 16,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 17,
+      },
+      {
+        color: 1,
+        slot_id: 20,
+        time: 18,
+      },
+      {
+        color: 1,
+        slot_id: 16,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 7,
+        time: 20,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 21,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 22,
+      },
+    ],
+    [
+      {
+        color: 0,
+        slot_id: 0,
+        time: 8,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 9,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 10,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 11,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 12,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 13,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 14,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 15,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 16,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 17,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 18,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 19,
+      },
+      {
+        color: 1,
+        slot_id: 13,
+        time: 20,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 21,
+      },
+      {
+        color: 0,
+        slot_id: 0,
+        time: 22,
+      },
+    ],
+  ];
 
-
-
-const table = [
-  [
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 9
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 10
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 11
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 12
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 13
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 14
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 15
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 16
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 17
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 18
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 14,
-          "time": 20
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 21
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 22
-      }
-  ],
-  [
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 9
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 10
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 11
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 12
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 13
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 14
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 15
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 16
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 17
-      },
-      {
-          "color": 1,
-          "slot_id": 24,
-          "time": 18
-      },
-      {
-          "color": 1,
-          "slot_id": 15,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 8,
-          "time": 20
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 21
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 22
-      }
-  ],
-  [
-      {
-          "color": 2,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 9
-      },
-      {
-          "color": 2,
-          "slot_id": 37,
-          "time": 10
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 11
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 12
-      },
-      {
-          "color": 2,
-          "slot_id": 32,
-          "time": 13
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 14
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 15
-      },
-      {
-          "color": 1,
-          "slot_id": 28,
-          "time": 16
-      },
-      {
-          "color": 1,
-          "slot_id": 23,
-          "time": 17
-      },
-      {
-          "color": 1,
-          "slot_id": 17,
-          "time": 18
-      },
-      {
-          "color": 1,
-          "slot_id": 10,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 6,
-          "time": 20
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 21
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 22
-      }
-  ],
-  [
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 2,
-          "slot_id": 38,
-          "time": 9
-      },
-      {
-          "color": 2,
-          "slot_id": 39,
-          "time": 10
-      },
-      {
-          "color": 2,
-          "slot_id": 35,
-          "time": 11
-      },
-      {
-          "color": 2,
-          "slot_id": 33,
-          "time": 12
-      },
-      {
-          "color": 1,
-          "slot_id": 31,
-          "time": 13
-      },
-      {
-          "color": 1,
-          "slot_id": 30,
-          "time": 14
-      },
-      {
-          "color": 1,
-          "slot_id": 29,
-          "time": 15
-      },
-      {
-          "color": 1,
-          "slot_id": 27,
-          "time": 16
-      },
-      {
-          "color": 1,
-          "slot_id": 22,
-          "time": 17
-      },
-      {
-          "color": 1,
-          "slot_id": 18,
-          "time": 18
-      },
-      {
-          "color": 1,
-          "slot_id": 11,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 9,
-          "time": 20
-      },
-      {
-          "color": 2,
-          "slot_id": 4,
-          "time": 21
-      },
-      {
-          "color": 2,
-          "slot_id": 3,
-          "time": 22
-      }
-  ],
-  [
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 9
-      },
-      {
-          "color": 2,
-          "slot_id": 36,
-          "time": 10
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 11
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 12
-      },
-      {
-          "color": 2,
-          "slot_id": 34,
-          "time": 13
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 14
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 15
-      },
-      {
-          "color": 1,
-          "slot_id": 26,
-          "time": 16
-      },
-      {
-          "color": 1,
-          "slot_id": 21,
-          "time": 17
-      },
-      {
-          "color": 1,
-          "slot_id": 19,
-          "time": 18
-      },
-      {
-          "color": 1,
-          "slot_id": 12,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 5,
-          "time": 20
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 21
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 22
-      }
-  ],
-  [
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 9
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 10
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 11
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 12
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 13
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 14
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 15
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 16
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 17
-      },
-      {
-          "color": 1,
-          "slot_id": 20,
-          "time": 18
-      },
-      {
-          "color": 1,
-          "slot_id": 16,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 7,
-          "time": 20
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 21
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 22
-      }
-  ],
-  [
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 8
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 9
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 10
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 11
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 12
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 13
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 14
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 15
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 16
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 17
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 18
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 19
-      },
-      {
-          "color": 1,
-          "slot_id": 13,
-          "time": 20
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 21
-      },
-      {
-          "color": 0,
-          "slot_id": 0,
-          "time": 22
-      }
-  ]
-]
-
-
-
-
-
-
-=======
-  console.log(table);
->>>>>>> 62f0eff0c8d98c849f645b3b98cee3e7e11af6ed
   const onClickSlot = (dayIndex, hourIndex) => {
-    switch (typeSelection) {  
+    switch (typeSelection) {
       case "Consultations":
         dispatch(setManagerLoading(true));
         return updateSlot(managerId, weekId, dayIndex, table[dayIndex][hourIndex].time, 1)
@@ -632,7 +620,7 @@ const table = [
         break;
     }
   };
-  
+
   useEffect(() => {
     dispatch(getManagerCurrentWorkWeek(1));
   }, []);
@@ -652,7 +640,6 @@ const table = [
       </div>
       {console.log(table)}
       <TableCheck table={table} onClickSlotFn={onClickSlot} />
-    
     </section>
   );
 };
