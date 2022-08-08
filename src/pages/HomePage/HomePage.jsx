c06d148f0aa2b781544acf4fbff7220f4b6424c7
Max Popsuy <maxim09p@gmail.com>
2022-08-08 09:47:15 +0300

    deleted modals

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,7 +12,6 @@ const HomePage = () => {
           { text: "admin", path: "admin/1/users/" },
           { text: "manager", path: "manager/1/planning/" },
           { text: "confirmator", path: "confirmator/1/" },
-          { text: "modals", path: path.modals },
         ]}
       />
       <section></section>
