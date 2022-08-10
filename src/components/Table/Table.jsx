444aa28208ea5d4bf9e42a32fae02d7da162d25a
Igoryakib <Igoryakib@gmail.com>
2022-08-10 23:07:46 +0300

    Added loader

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,48 +2,61 @@ import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import styles from "./Table.module.scss";
 import TableItem from "../TableItem/TableItem";
+import { useSelector } from "react-redux";
+import { isManagerLoading } from "../../redux/manager/manager-selectors";
+import { getCallerLoading } from "../../redux/caller/caller-selectors";
+import { TailSpin } from "react-loader-spinner";
 
 const Table = ({ weekId, table, onClickSlotFn, consultation, caller }) => {
   useEffect(() => {});
+  const managerLoading = useSelector(isManagerLoading);
+  const callerLoading = useSelector(getCallerLoading);
   return (
-    <ul className={styles.table}>
-      {table.map((day, dayIndex) => {
-        return day.map((item, hourIndex) => {
-          return (
-            <Fragment key={hourIndex}>
-              {caller ? (
-                <TableItem
-                  onClickFn={() => onClickSlotFn()}
-                  data={item.time}
-                  weekId={weekId}
-                  colorId={item.amount}
-                  caller={caller}
-                  hourIndex={table[dayIndex][hourIndex].time}
-                  slotId={item.slot_id}
-                  dayIndex={dayIndex}
-                />
-              ) : consultation ? (
-                <TableItem
-                  data={item.time}
-                  colorId={item.color}
-                  dayIndex={dayIndex}
-                  hourIndex={hourIndex}
-                  consultation
-                  slotId={item.slot_id}
-                  onClickBtnStart={() => onClickSlotFn(dayIndex, hourIndex)}
-                />
-              ) : (
-                <TableItem
-                  onClickFn={() => onClickSlotFn(dayIndex, hourIndex)}
-                  data={item.time}
-                  colorId={item.color}
-                />
-              )}
-            </Fragment>
-          );
-        });
-      })}
-    </ul>
+    <div className={styles.wrapperTable}>
+      {(managerLoading || callerLoading) && (
+          <div className={styles.spinner}>
+            <TailSpin height="57" width="57" color="#999DFF" />
+          </div>
+        )}
+      <ul className={styles.table}>
+        {table.map((day, dayIndex) => {
+          return day.map((item, hourIndex) => {
+            return (
+              <Fragment key={hourIndex}>
+                {caller ? (
+                  <TableItem
+                    onClickFn={() => onClickSlotFn()}
+                    data={item.time}
+                    weekId={weekId}
+                    colorId={item.amount}
+                    caller={caller}
+                    hourIndex={table[dayIndex][hourIndex].time}
+                    slotId={item.slot_id}
+                    dayIndex={dayIndex}
+                  />
+                ) : consultation ? (
+                  <TableItem
+                    data={item.time}
+                    colorId={item.color}
+                    dayIndex={dayIndex}
+                    hourIndex={hourIndex}
+                    consultation
+                    slotId={item.slot_id}
+                    onClickBtnStart={() => onClickSlotFn(dayIndex, hourIndex)}
+                  />
+                ) : (
+                  <TableItem
+                    onClickFn={() => onClickSlotFn(dayIndex, hourIndex)}
+                    data={item.time}
+                    colorId={item.color}
+                  />
+                )}
+              </Fragment>
+            );
+          });
+        })}
+      </ul>
+    </div>
   );
 };
 
--- src/redux/caller/caller-selectors.js
+++ src/redux/caller/caller-selectors.js
@@ -3,6 +3,7 @@ const getTable = (state) => state.caller.week.slots;
 const getTypeSelection = (state) => state.caller.typeActionSelection;
 const getCallerId = (state) => state.caller.callerId;
 const getWeekId = (state) => state.caller.week.weekId;
+const getCallerLoading = (state) => state.caller.CallerLoading;
 
 export {
   getDate,
@@ -10,4 +11,5 @@ export {
   getTypeSelection,
   getCallerId,
   getWeekId,
+  getCallerLoading,
 };
