cd4d498a85554b3a23ce7ccf6c9a55ab74cf4839
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-08-15 12:29:31 +0300

    added favicon

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,19 @@
+import { Fade } from "react-awesome-reveal";
 import React from "react";
 import Header from "../../components/Header/Header";
-import styles from './HomePage.module.scss';
+import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
   return (
     <>
-      <Header
-        endpoints={[
-          { text: "superadmin", path: "superadmin/users/" },
-        ]}
-      />
-      <section className={styles.home}><p className={styles.error}><span>404</span>Not Found</p></section>
+      <Header endpoints={[{ text: "superadmin", path: "superadmin/users/" }]} />
+      <section className={styles.home}>
+     <Fade triggerOnce duration={250} direction="down">
+          <p className={styles.error}>
+            <span>404</span>Not Found
+          </p>
+     </Fade>
+      </section>
     </>
   );
 };
